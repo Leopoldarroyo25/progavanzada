9648850dd597dfc897e8f0a9f07b8f9aeefb38fd
Leopoldarroyo25 <55461684+Leopoldarroyo25@users.noreply.github.com>
2019-09-23 23:17:48 -0500

    Add files via upload

---
+++ ejercicio1.py
@@ -0,0 +1,6 @@
+nombre= input ('inserta tu nombre')
+edad= int (input ('ingresa tu edad'))
+Estatura= float (input('ingresa tu estatura'))
+
+
+print('mi nombre es:', nombre, 'naci en:', 2019-edad , 'y tengo una estura de', Estatura)
