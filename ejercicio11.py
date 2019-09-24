5f50f3b518afdd1c161b527eeb49584245780941
Leopoldarroyo25 <55461684+Leopoldarroyo25@users.noreply.github.com>
2019-09-23 23:42:24 -0500

    Add files via upload

---
+++ ejercicio11.py
@@ -0,0 +1,6 @@
+#ejercicio11
+#(In the United States, fuel efficiency for vehicles is normally expressed in miles-per-gallon(MPG). In Mexico, fuell efficiency is normally expressed in lites-per-hundred MPG to (L/100km). Then create a program that reads a value from the user in American units and displays the equivalent fue in Mexican units.)
+USD=int(input('introduzca cantidad en sistema US'))
+SI=235.21
+operacion= USD*SI
+print('Cantidad es:', operacion, 'L/100km') 
