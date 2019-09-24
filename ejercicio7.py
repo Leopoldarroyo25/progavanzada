429ab51f9427b02756eb9fddb56d53e610291d8e
Leopoldarroyo25 <55461684+Leopoldarroyo25@users.noreply.github.com>
2019-09-23 23:19:29 -0500

    Add files via upload

---
+++ ejercicio7.py
@@ -0,0 +1,6 @@
+#(Ejercicio 7.)
+#(Escruba un programa que lea un numero positivo(n). insertado por el usuario y despúes despliegue la suma de todos los eneteros desde 1 hasta n positivos puede calculado usando la formula.)
+
+n= int(input('ingresa tu numero'))
+operacion= int((n*(n+ 1))/2) 
+print(operacion)
