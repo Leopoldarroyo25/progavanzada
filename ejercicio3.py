373654e76e0eb80b90d99293b6ca51a13c72ab85
Leopoldarroyo25 <55461684+Leopoldarroyo25@users.noreply.github.com>
2019-09-23 23:18:24 -0500

    Add files via upload

---
+++ ejercicio3.py
@@ -0,0 +1,4 @@
+print ("Gomora\nMexico\nCDMX\n")
+
+print("                                       Leopoldo Lopez Arroyo\n                                     pinos 6\n                                         Ciudad de Mexico")
+ 
