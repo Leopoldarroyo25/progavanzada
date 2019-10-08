3156845856fe3827edcc2833cc3cf74854acfc38
Leopoldarroyo25 <55461684+Leopoldarroyo25@users.noreply.github.com>
2019-10-07 22:01:50 -0500

    Add files via upload

---
+++ ejercicio15.py
@@ -0,0 +1,15 @@
+# ejercicio 15
+#In this exercise, you will create a program that begins by reading a measurement
+#in feet from the user. Then your program should display the equivalent distance in
+#inches, yards and miles. Use the Internet to look up the necessary conversion factors
+#if you don’t have them memorized.
+#pies=float(input('ingrese la cantidad de pies:'))
+
+pulgadas=pies*12
+yardas=pies/3
+millas=pies/5280
+
+
+print('equivalente en pulgadas', pulgadas)
+print('equivalente en yardas', yardas)
+print('equivalente en millas', millas)
