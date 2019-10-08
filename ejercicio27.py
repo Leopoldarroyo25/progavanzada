cb376e30e3c445b9deb643f46ebe9b5c65d99a5e
Leopoldarroyo25 <55461684+Leopoldarroyo25@users.noreply.github.com>
2019-10-07 22:05:47 -0500

    Add files via upload

---
+++ ejercicio27.py
@@ -0,0 +1,14 @@
+#ejercicio27
+#Write a program that computes the body mass index (BMI) of an individual. Your
+#program should begin by reading a height and weight from the user. Then it should
+#use one of the following two formulas to compute the BMI before displaying it. If
+#you read the height in inches and the weight in pounds then body mass index is
+#computed using the following formula:
+#BMI = (weight/ (height × height)) × 703.
+#If you read the height in meters and the weight in kilograms then body mass index
+#is computed using this slightly simpler formula:
+#BMI = weight/ height × height
+a= float(input('ingrese altura en metros \n'))
+p= float(input('ingrrese peso en kg \n'))
+imc= p/(a * a)
+print('Su IMC es %.4f' % imc)
