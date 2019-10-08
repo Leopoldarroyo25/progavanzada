d2989029785da84d5f6fde2f86461224f7e46923
Leopoldarroyo25 <55461684+Leopoldarroyo25@users.noreply.github.com>
2019-10-07 22:03:31 -0500

    Add files via upload

---
+++ ejercicio20.py
@@ -0,0 +1,18 @@
+#ejercicio 20
+#The ideal gas law is a mathematical approximation of the behavior of gasses as
+#pressure, volume and temperature change. It is usually stated as:
+#PV = nRT
+#where P is the pressure in Pascals, V is the volume in liters, n is the amount of
+#substance in moles, R is the ideal gas constant, equal to 8.314 mol K J , and T is the
+#temperature in degrees Kelvin.
+#Write a program that computes the amount of gas in moles when the user supplies
+#the pressure, volume and temperature. Test your program by determining the number
+#of moles of gas in a SCUBA tank. A typical SCUBA tank holds 12 liters of gas at
+#a pressure of 20,000,000 Pascals (approximately 3,000 PSI). Room temperature is
+#approximately 20 degrees Celsius or 68 degrees Fahrenheit.
+R=8.314
+T=int(input('ingrese Temperatura \n'))
+P=int(input('ingrese precion en pascales \n'))
+V=int(input('ingrese volumen en litro \n'))
+operacion= (P*V)/(R*T)
+print('tu resultado en moles es', operacion)
